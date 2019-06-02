261f423585204501552513c37da868eb324103e3
khugar <ksangon98@gmail.com>
2019-06-02 19:51:28 +0200

    Clase Ticket

--- app/src/main/java/com/example/client_restaurant/Ticket.java
+++ app/src/main/java/com/example/client_restaurant/Ticket.java
@@ -1,4 +1,37 @@
 package com.example.client_restaurant;
 
 public class Ticket {
+
+    private Integer idTicket;
+    private Float totalPrice;
+    private Integer idTable;
+
+    public Ticket(Float totalPrice, Integer idTable) {
+        this.totalPrice = totalPrice;
+        this.idTable = idTable;
+    }
+
+    public Integer getIdTicket() {
+        return idTicket;
+    }
+
+    public void setIdTicket(Integer idTicket) {
+        this.idTicket = idTicket;
+    }
+
+    public Float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Integer getIdTable() {
+        return idTable;
+    }
+
+    public void setIdTable(Integer idTable) {
+        this.idTable = idTable;
+    }
 }
